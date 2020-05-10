dc0412e7d355eb4e575c079f9086b4fb71e601af
Javier Juarez <javier.juarez@clarity.ai>
2020-05-11 01:44:08 +0200

    Avoid to use a log library for this

--- src/main/java/ai/clarity/sre/docker/CGroupsTest.java
+++ src/main/java/ai/clarity/sre/docker/CGroupsTest.java
@@ -1,23 +1,33 @@
 package ai.clarity.sre.docker;
 
+import java.text.NumberFormat;
 import java.util.Vector;
 
 public class CGroupsTest {
+	private static final int MB        = 1024 * 1024;
+  private static final String suffix = " MB";
 
-	private static final int MB = 1024 * 1024;
+	public static void main(final String[] args) throws Exception {
+	  Runtime runtime = Runtime.getRuntime();
 
-	public static void main(final String[] args) {
-		System.out.println("Available processors: " + Runtime.getRuntime().availableProcessors());
-		System.out.println("Max memory:           " + (Runtime.getRuntime().maxMemory() / MB));
-		System.out.println("Total memory:         " + (Runtime.getRuntime().totalMemory() / MB));
+	  final NumberFormat formater = NumberFormat.getInstance();
+
+	  final int processors       = runtime.availableProcessors();
+	  final long maxMemory       = runtime.maxMemory();
+	  final long allocatedMemory = runtime.totalMemory();
+	  final long freeMemory      = runtime.freeMemory();
+
+		System.out.println("Available processors: " + processors);
+		System.out.println("Max memory:           " + formater.format(maxMemory / MB) + suffix);
+		System.out.println("Allocated memory:     " + formater.format(allocatedMemory / MB) + suffix);
+		System.out.println("Free memory:          " + formater.format(freeMemory / MB) + suffix);
 
 		@SuppressWarnings("rawtypes")
 		final Vector v = new Vector();
 		while (true) {
 			final byte b[] = new byte[MB];
-
 			v.add(b);
-			System.out.println("  free memory: " + (Runtime.getRuntime().freeMemory() / MB));
+			System.out.println("  free memory: " + formater.format(runtime.freeMemory() / MB) + suffix);
 		}
 	}
 }
